65f0be2ca47d761f309443acded4d6c57a21c151
Mansi Sharma <86704359+Mansi-2709@users.noreply.github.com>
2025-12-19 08:25:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,7 @@ import warnings
 warnings.filterwarnings('ignore')
 st.set_page_config(page_title="Bank Customer Churn Prediction & Customer Retention Dashboard", page_icon=":bank:",layout="wide")
 st.title("🏦 Bank Customer Churn Prediction & Customer Retention Dashboard")
+with st.expander('Data'):
+  st.write('**Raw Data**')
+  df = pd.read_csv('https://raw.githubusercontent.com/Mansi-2709/BankChurn/refs/heads/master/Customer-Churn-Records.csv')
+  df
