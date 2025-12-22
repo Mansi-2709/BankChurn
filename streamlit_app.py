225301b3ac2aaeda5e252e589a3bbe204ffa00c5
Mansi Sharma <86704359+Mansi-2709@users.noreply.github.com>
2025-12-22 19:24:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,9 @@ import plotly.express as px
 import numpy as np
 import os
 import warnings
+from sklearn.preprocessing import LabelEncoder
+from sklearn.ensemble import RandomForestClassifier
+
 warnings.filterwarnings('ignore')
 st.set_page_config(page_title="Bank Customer Churn Prediction & Customer Retention Dashboard", page_icon=":bank:",layout="wide")
 st.title("🏦 Bank Customer Churn Prediction & Customer Retention Dashboard")
@@ -17,6 +20,10 @@ with st.expander('X dataframe'):
   col_X=['Geography', 'Gender', 'Age', 'Tenure', 'Balance', 'NumOfProducts', 'IsActiveMember', 'Complain']
   X=df[col_X]
   X
+with st.expander('Target Variable'):
+  st.write('**y**')
+  y = df[['Exited']]
+  y
 
 
 with st.expander('Input your data'):
@@ -46,3 +53,18 @@ bank_churn = pd.concat([input_df,X], axis=0)
 with st.expander('Your input data'):
   st.write('**Input Data**')
   input_df
+  encoder = LabelEncoder()
+	bank_churn['Geography'] = encoder.fit_transform(bank_churn['Geography'])
+  bank_churn['Gender'] = encoder.fit_transform(bank_churn['Gender'])
+  bank_churn['Balance_exponential'] = bank_churn.Balance**(1/1.2)
+  X = bank_churn[1:]
+  clf = RandomForestClassifier(class_weight = {1:0,1: 4}, criterion = 'entropy', n_estimators = 30, random_state=42)
+  clf.fit(X,y)
+  input_df=bank_churn[:1]
+	y_pred=clf.predict(input_df)
+
+with st.expander('Predictions'):
+  if y_pred == 1:
+    st.write('The Customer will Exit.')
+  else :
+    st.write('The Customer will Not exit.')
