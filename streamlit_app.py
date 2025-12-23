ae9c7272d0e92ad4188981ca980a0e303493528d
Mansi Sharma <86704359+Mansi-2709@users.noreply.github.com>
2025-12-23 17:41:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,12 @@
 import streamlit as st
-import pandas as pd
-import plotly.express as px
-import numpy as np
-import os
 import warnings
-from sklearn.preprocessing import LabelEncoder
-from sklearn.ensemble import RandomForestClassifier
+
 
 warnings.filterwarnings('ignore')
-welcome = st.Page("streamlit_app.py", title="Welcome", icon="🏦")
-prediction = st.Page("pages/Predictions.py", title="Predictions", icon="📊")
-dashboard = st.Page("pages/Dashboard.py", title="Dashboard", icon="📊")
-pg = st.navigation([welcome, prediction, dashboard])
+if "page_initialized" not in st.session_state:
+    st.session_state.page_initialized = True
+    welcome = st.Page("streamlit_app.py", title="Welcome", icon="🏦")
+    prediction = st.Page("pages/Predictions.py", title="Predictions", icon="📊")
+    dashboard = st.Page("pages/Dashboard.py", title="Dashboard", icon="📊")
+    pg = st.navigation([welcome, prediction, dashboard])
+    pg.run()
