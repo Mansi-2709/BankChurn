633c70fccd6aef20c5622435a45e0c853dacb55d
Mansi Sharma <86704359+Mansi-2709@users.noreply.github.com>
2025-12-23 18:22:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,11 @@ import warnings
 
 
 warnings.filterwarnings('ignore')
-if "page_initialized" not in st.session_state:
-    st.session_state.page_initialized = True
-    welcome = st.Page("streamlit_app.py", title="Welcome", icon="🏦")
-    prediction = st.Page("pages/Predictions.py", title="Predictions", icon="📊")
-    dashboard = st.Page("pages/Dashboard.py", title="Dashboard", icon="📊")
-    pg = st.navigation([welcome, prediction, dashboard])
-    pg.run()
+st.set_page_config(
+    page_title="Home",
+    page_icon="🏠",
+    layout="wide"
+)
+
+st.title("Home Page")
+st.write("Welcome to the application.")
