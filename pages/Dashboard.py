3d78f1d01298edce99790b624b9344f3256dbcc1
Mansi Sharma <86704359+Mansi-2709@users.noreply.github.com>
2025-12-25 09:51:33 +0530

    Update Dashboard.py

--- pages/Dashboard.py
+++ pages/Dashboard.py
@@ -1,1 +1,10 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import numpy as np
+import os
+import warnings
+warnings.filterwarnings('ignore')
 
+st.set_page_config(page_title="Bank Churn Dashboard", page_icon=":droplet:",layout="wide")
+df = pd.read_csv('')
