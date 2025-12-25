7ad8ba28c19e177987ff263d20205d7b933bb161
Mansi Sharma <86704359+Mansi-2709@users.noreply.github.com>
2025-12-25 13:28:49 +0530

    Update Dashboard.py

--- pages/Dashboard.py
+++ pages/Dashboard.py
@@ -6,5 +6,5 @@ import os
 import warnings
 warnings.filterwarnings('ignore')
 
-st.set_page_config(page_title="Bank Churn Dashboard", page_icon=":droplet:",layout="wide")
-df = pd.read_csv('')
+st.set_page_config(page_title="Bank Churn Dashboard", page_icon=":money_with_wings:",layout="wide")
+df = pd.read_csv('https://raw.githubusercontent.com/Mansi-2709/BankChurn/refs/heads/master/Customer-Churn-Records.csv')
